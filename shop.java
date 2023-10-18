10f069e75d3523ca83ff06a5257162e1d9ebfdc4
Nsunithareddy1111 <144465243+Nsunithareddy1111@users.noreply.github.com>
2023-10-18 09:27:29 +0530

    Create shop.java

---
+++ shop.java
@@ -0,0 +1,7 @@
+echo "# ShopManagement" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Nsunithareddy1111/ShopManagement.git
+git push -u origin main
